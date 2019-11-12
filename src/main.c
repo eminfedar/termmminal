32117ac9647751613d3af2e0a10c3de9b7052eaf
Emin Fedar <eminfedar@gmail.com>
2019-11-12 21:48:47 +0300

    [added] CTRL+C (SIGINT) does not close terminal.

--- src/main.c
+++ src/main.c
@@ -2,9 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <signal.h>
+
+void exitMain(){
+	printf("\n> ");
+	fflush(stdout);
+}
 
 int main()
 {
+	signal(SIGINT, exitMain);
+
 	setlocale(LC_ALL, "Turkish"); 
 	startPrompting();
 
